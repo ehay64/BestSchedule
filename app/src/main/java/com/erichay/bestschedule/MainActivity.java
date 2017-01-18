157bb79934c67e17b1fee169330a2970b833b28d
Eric Hay <erichay@live.ca>
2017-01-18 11:26:46 -0800

    Initial Commit

---
+++ app/src/main/java/com/erichay/bestschedule/MainActivity.java
@@ -0,0 +1,15 @@
+package com.erichay.bestschedule;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity
+{
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
