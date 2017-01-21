a148f097a3783a74d92f78b15e8b5cb0c322b9ce
Eric Hay <erichay@live.ca>
2017-01-21 14:49:18 -0800

    Datetime Picking now Works

--- app/src/main/java/com/erichay/bestschedule/EditTask.java
+++ app/src/main/java/com/erichay/bestschedule/EditTask.java
@@ -1,14 +1,27 @@
 package com.erichay.bestschedule;
 
+import android.app.DialogFragment;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toolbar;
 
+import java.util.Date;
+
 public class EditTask extends Activity
 {
     private long taskId;
+
+    private int year;
+    private int month;
+    private int day;
+
+    private int hour;
+    private int minute;
+
+    private Date date;
 
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -32,14 +45,33 @@ public class EditTask extends Activity
             ((EditText)findViewById(R.id.edit_name)).setText(task.getName());
             ((EditText)findViewById(R.id.edit_hours)).setText(Integer.toString(task.getHours()));
             ((EditText)findViewById(R.id.edit_date)).setText(task.getDueDate().toString());
-
+            date = task.getDueDate();
         }
     }
 
     public void getDate(View v)
     {
+        DialogFragment dateFragment = new DatePickerFragment();
+        dateFragment.show(getFragmentManager(), "datePicker");
+    }
 
+    public void setDate(int y, int m, int d)
+    {
+        year = y;
+        month = m;
+        day = d;
 
+        DialogFragment timeFragment = new TimePickerFragment();
+        timeFragment.show(getFragmentManager(), "timePicker");
+    }
+
+    public void setTime(int h, int m)
+    {
+        hour = h;
+        minute = m;
+
+        date = new Date(year - 1900, month, day, hour, minute);
+        ((EditText)findViewById(R.id.edit_date)).setText(date.toString());
     }
 
 }
