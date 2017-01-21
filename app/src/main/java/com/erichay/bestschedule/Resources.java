9cf16e7aec75c8fe3c4c34b8e45889c267060302
Eric Hay <erichay@live.ca>
2017-01-21 14:26:28 -0800

    Added Save and load methods

--- app/src/main/java/com/erichay/bestschedule/Resources.java
+++ app/src/main/java/com/erichay/bestschedule/Resources.java
@@ -1,7 +1,13 @@
 package com.erichay.bestschedule;
 
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import android.content.Context;
 
 public class Resources
 {
@@ -31,5 +37,43 @@ public class Resources
         }
 
         return null;
+    }
+
+    public static void save()
+    {
+        try
+        {
+            FileOutputStream writeStream = new FileOutputStream("taskList");
+            ObjectOutputStream writeObjectStream = new ObjectOutputStream(writeStream);
+            writeObjectStream.writeObject(tasks);
+            //Close streams
+            writeObjectStream.close();
+            writeStream.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void load()
+    {
+        try
+        {
+            FileInputStream readStream = new FileInputStream("taskList");
+            ObjectInputStream readObjectStream = new ObjectInputStream(readStream);
+            tasks = (ArrayList<Task>) readObjectStream.readObject();
+            //Close streams
+            readObjectStream.close();
+            readStream.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+        catch(ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
